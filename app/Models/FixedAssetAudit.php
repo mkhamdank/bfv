1d32a6d0b0f86ce525daf47ce1984f5433112a00
DESKTOP-MIS10\YMPI <fakhrizal.ihza.mahendra@music.yamaha.com>
2024-08-15 12:14:01 +0700

    NI - FA bug

--- app/Models/FixedAssetAudit.php
+++ app/Models/FixedAssetAudit.php
@@ -1,12 +1,14 @@
 <?php
 
-namespace App;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class FixedAssetAudit extends Model
 {
+
+	protected $table = 'fixed_asset_audits';
 	protected $fillable = ['period','category','location','sap_number','asset_name','asset_section','asset_map','asset_images','result_images','note','availability','asset_condition','label_condition','usable_condition','map_condition','asset_image_condition','pic','status','result_video','checked_by','checked_date','remark', 'audit_type','created_by',
 	];
 
