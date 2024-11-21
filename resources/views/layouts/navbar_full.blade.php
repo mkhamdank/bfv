96921c21c93aa1714301eef33c42c43fb9d9a2df
DESKTOP-MIS10\YMPI <fakhrizal.ihza.mahendra@music.yamaha.com>
2024-11-21 11:08:00 +0700

    Mk - navbar

---
+++ resources/views/layouts/navbar_full.blade.php
@@ -0,0 +1,17 @@
+<nav class="main-header navbar navbar-expand bg-primary">
+    <!-- Left navbar links -->
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a id="toggle-sidebar" class="nav-link ms-3 text-light" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
+        </li>
+    </ul>
+
+    <!-- SEARCH FORM -->
+    <div class="form-inline ml-3">
+
+        <div class="input-group input-group-sm">     
+        </div>
+
+    </div>
+
+</nav>
