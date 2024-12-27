1168faede49a18fcc737dbd0a329620e557d65ec
DESKTOP-MIS10\YMPI <fakhrizal.ihza.mahendra@music.yamaha.com>
2024-12-27 07:34:44 +0700

    MK - bfv

--- app/Http/Controllers/GeneralAffairController.php
+++ app/Http/Controllers/GeneralAffairController.php
@@ -736,21 +736,21 @@ class GeneralAffairController extends Controller
             $fuel = $request->input('fuel');
             $tanggal = date('Y-m-d-H-i-s');
 
-            // $_IP_ADDRESS = $_SERVER['REMOTE_ADDR'];
+            $_IP_ADDRESS = $_SERVER['REMOTE_ADDR'];
 
-            // $_PERINTAH = "arp -a $_IP_ADDRESS";
-            // ob_start();
-            // system($_PERINTAH);
-            // $_HASIL = ob_get_contents();
-            // ob_clean();
-            // $_PECAH = strstr($_HASIL, $_IP_ADDRESS);
+            $_PERINTAH = "arp -a $_IP_ADDRESS";
+            ob_start();
+            system($_PERINTAH);
+            $_HASIL = ob_get_contents();
+            ob_clean();
+            $_PECAH = strstr($_HASIL, $_IP_ADDRESS);
             
-            // if ($_PECAH == FALSE) {
-            //     $_HASIL = $_IP_ADDRESS;
-            // }else{
-            //     $_PECAH_STRING = explode($_IP_ADDRESS, str_replace(" ", "", $_PECAH));
-            //     $_HASIL = substr($_PECAH_STRING[1], 0, 17);               
-            // }
+            if ($_PECAH == FALSE) {
+                $_HASIL = $_IP_ADDRESS;
+            }else{
+                $_PECAH_STRING = explode($_IP_ADDRESS, str_replace(" ", "", $_PECAH));
+                $_HASIL = substr($_PECAH_STRING[1], 0, 17);               
+            }
 
             $tujuan_upload = 'images/absensi';
 
@@ -776,69 +776,69 @@ class GeneralAffairController extends Controller
             }
             $file_upload_foto_odometer = json_encode($data_foto_odometer);
 
-            // $url = "https://locationiq.org/v1/reverse.php?key=pk.456ed0d079b6f646ad4db592aa541ba0&lat=".$latitude."&lon=".$longitude."&format=json";
-            // $curlHandle = curl_init();
-            // curl_setopt($curlHandle, CURLOPT_URL, $url);
-            // curl_setopt($curlHandle, CURLOPT_HEADER, 0);
-            // curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, 1);
-            // curl_setopt($curlHandle, CURLOPT_SSL_VERIFYHOST, 2);
-            // curl_setopt($curlHandle, CURLOPT_SSL_VERIFYPEER, 0);
-            // curl_setopt($curlHandle, CURLOPT_TIMEOUT,30);
-            // curl_setopt($curlHandle, CURLOPT_POST, 1);
-            // $results = curl_exec($curlHandle);
-            // curl_close($curlHandle);
+            $url = "https://locationiq.org/v1/reverse.php?key=pk.456ed0d079b6f646ad4db592aa541ba0&lat=".$latitude."&lon=".$longitude."&format=json";
+            $curlHandle = curl_init();
+            curl_setopt($curlHandle, CURLOPT_URL, $url);
+            curl_setopt($curlHandle, CURLOPT_HEADER, 0);
+            curl_setopt($curlHandle, CURLOPT_RETURNTRANSFER, 1);
+            curl_setopt($curlHandle, CURLOPT_SSL_VERIFYHOST, 2);
+            curl_setopt($curlHandle, CURLOPT_SSL_VERIFYPEER, 0);
+            curl_setopt($curlHandle, CURLOPT_TIMEOUT,30);
+            curl_setopt($curlHandle, CURLOPT_POST, 1);
+            $results = curl_exec($curlHandle);
+            curl_close($curlHandle);
 
-            // $addrs = json_encode($results);
-            // $loc2 = explode('\"',$addrs);
+            $addrs = json_encode($results);
+            $loc2 = explode('\"',$addrs);
 
-            // $keyVillage = array_search('village', $loc2);
-            // $keyResidential = array_search('residential', $loc2);
-            // $keyHamlet = array_search('hamlet', $loc2);
-            // $keyNeighbourhood = array_search('neighbourhood', $loc2);
+            $keyVillage = array_search('village', $loc2);
+            $keyResidential = array_search('residential', $loc2);
+            $keyHamlet = array_search('hamlet', $loc2);
+            $keyNeighbourhood = array_search('neighbourhood', $loc2);
 
-            // $keyStateDistrict = array_search('state_district', $loc2);
-            // $keyCity = array_search('city', $loc2);
-            // $keyCounty = array_search('county', $loc2);
+            $keyStateDistrict = array_search('state_district', $loc2);
+            $keyCity = array_search('city', $loc2);
+            $keyCounty = array_search('county', $loc2);
 
-            // $keyState = array_search('state', $loc2);
-            // $keyPostcode = array_search('postcode', $loc2);
-            // $keyCountry = array_search('country', $loc2);
+            $keyState = array_search('state', $loc2);
+            $keyPostcode = array_search('postcode', $loc2);
+            $keyCountry = array_search('country', $loc2);
 
-            // if($keyVillage && $loc2[$keyVillage+2] != ":"){
-            //     $village = $loc2[$keyVillage+2];
-            // }
-            // else if($keyResidential && $loc2[$keyResidential+2] != ":") {
-            //     $village = $loc2[$keyResidential+2];
-            // }
-            // else if($keyHamlet && $loc2[$keyHamlet+2] != ":") {
-            //     $village = $loc2[$keyHamlet+2];
-            // }
-            // else if($keyNeighbourhood && $loc2[$keyNeighbourhood+2] != ":") {
-            //     $village = $loc2[$keyNeighbourhood+2];
-            // }
-            // else{  
-            //     $village = null;
-            // }
+            if($keyVillage && $loc2[$keyVillage+2] != ":"){
+                $village = $loc2[$keyVillage+2];
+            }
+            else if($keyResidential && $loc2[$keyResidential+2] != ":") {
+                $village = $loc2[$keyResidential+2];
+            }
+            else if($keyHamlet && $loc2[$keyHamlet+2] != ":") {
+                $village = $loc2[$keyHamlet+2];
+            }
+            else if($keyNeighbourhood && $loc2[$keyNeighbourhood+2] != ":") {
+                $village = $loc2[$keyNeighbourhood+2];
+            }
+            else{  
+                $village = null;
+            }
 
-            // if ($keyStateDistrict && $loc2[$keyStateDistrict + 2] != ":") {
-            //     $city = $loc2[$keyStateDistrict + 2];
-            // }
-            // else if($keyCity && $loc2[$keyCity + 2] != ":") {
-            //     $city = $loc2[$keyCity + 2];
-            // }
-            // else if($keyCounty && $loc2[$keyCounty+2] != ":") {
-            //     $city = $loc2[$keyCounty+2];
-            // }
-            // else{  
-            //     $city = null;
-            // }
+            if ($keyStateDistrict && $loc2[$keyStateDistrict + 2] != ":") {
+                $city = $loc2[$keyStateDistrict + 2];
+            }
+            else if($keyCity && $loc2[$keyCity + 2] != ":") {
+                $city = $loc2[$keyCity + 2];
+            }
+            else if($keyCounty && $loc2[$keyCounty+2] != ":") {
+                $city = $loc2[$keyCounty+2];
+            }
+            else{  
+                $city = null;
+            }
 
-            // if($keyState && $loc2[$keyState + 2] != ":"){
-            //     $province = $loc2[$keyState + 2];
-            // }
-            // else{
-            //     $province = null;
-            // }
+            if($keyState && $loc2[$keyState + 2] != ":"){
+                $province = $loc2[$keyState + 2];
+            }
+            else{
+                $province = null;
+            }
 
             $create = DB::table('attendances')
             ->insert([
@@ -854,10 +854,10 @@ class GeneralAffairController extends Controller
                 'car' => $car,
                 'odometer' => $odometer,
                 'fuel' => $fuel,
-                // 'state' => $province,
-                // 'state_district' => $city,
-                // 'village' => $village,
-                // 'ip_address' => $_HASIL,
+                'state' => $province,
+                'state_district' => $city,
+                'village' => $village,
+                'ip_address' => $_HASIL,
                 'created_at' => date('Y-m-d H:i:s'),
                 'updated_at' => date('Y-m-d H:i:s')
             ]);
